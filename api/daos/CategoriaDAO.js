11b878e9e3cb66bb3d097f42f32b4a484973f804
morganaaraujo <56846947+morganaaraujo@users.noreply.github.com>
2021-07-27 23:45:02 -0300

    Ajuste ProdutoDAO e CategoriaDAO

--- api/daos/CategoriaDAO.js
+++ api/daos/CategoriaDAO.js
@@ -1,62 +1,23 @@
 module.exports = {
     async list() {
-        sails.getDatastore().sendNativeQuery('SELECT * FROM categorias;', [], function (err, rawResult) {
-            if (e) {
-                console.log(err)
-                return err
-            }
-            console.log(rawResult)
-            return rawResult;
-        });
+        return await sails.getDatastore().sendNativeQuery('SELECT * FROM categorias;', [])
     },
     async selectByDescricao(params) {
-        sails.getDatastore().sendNativeQuery("SELECT * FROM categorias WHERE descricao = '$1';", [params.descricao], function (err, rawResult) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-            console.log(rawResult)
-            return rawResult;
-        });
+        return await sails.getDatastore().sendNativeQuery("SELECT * FROM categorias WHERE descricao = '$1';", [params.descricao])
     },
     async create(params) {
-        sails.getDatastore().sendNativeQuery('SELECT id FROM categorias ORDER BY id DESC LIMIT 1;', [], function (e, firstResult) {
-            let lastId = firstResult.rows.length == 0 ? 0 : firstResult.rows[0].id
-            sails.getDatastore().sendNativeQuery('INSERT INTO produtos(id, descricao) VALUES($1, $2);', [lastId + 1, params.descricao], function (err, rawResult) {
-                    if (err) {
-                        console.log(err)
-                        return err
-                    }
-                    console.log(rawResult)
-                    return rawResult;
-            });
-        });
+        let result = await sails.getDatastore().sendNativeQuery('SELECT id FROM categorias ORDER BY id DESC LIMIT 1;')
+        let lastId = result.rowCount == 0 ? 0 : result.rows[0].id
+        return await sails.getDatastore().sendNativeQuery('INSERT INTO categorias(id, descricao) VALUES($1, $2);', [lastId + 1, params.descricao])
     },
     async selectById(params) {
-        sails.getDatastore().sendNativeQuery('SELECT * FROM categorias WHERE id = $1;', [params.id], function (err, rawResult) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-            console.log(rawResult)
-            return rawResult;
-        });
+        return await sails.getDatastore().sendNativeQuery('SELECT * FROM categorias WHERE id = $1;', [params.id])
     },
     async delete(params) {
-        sails.getDatastore().sendNativeQuery('DELETE FROM categorias WHERE id = $1;', [params.id], function (err) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-        });
+        return await sails.getDatastore().sendNativeQuery('DELETE FROM categorias WHERE id = $1;', [params.id])
     },
     async update(params) {
-        sails.getDatastore().sendNativeQuery('UPDATE categorias SET descricao = $2 WHERE id = $1;', [params.id, params.descricao], function (err, rawResult) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-        });
+        return await sails.getDatastore().sendNativeQuery('UPDATE categorias SET descricao = $2 WHERE id = $1;', [params.id, params.descricao])
     },
 
 }
--- api/daos/ProdutoDAO.js
+++ api/daos/ProdutoDAO.js
@@ -3,63 +3,23 @@ module.exports = {
         return await sails.getDatastore().sendNativeQuery('SELECT * FROM produtos WHERE id = $1;', [id])
     },
     async list() {
-        sails.getDatastore().sendNativeQuery('SELECT * FROM produtos;', [], function (err, rawResult) {
-            if (e) {
-                console.log(err)
-                return err
-            }
-            console.log(rawResult)
-            return rawResult;
-        });
+        return await sails.getDatastore().sendNativeQuery('SELECT * FROM produtos;', [])
     },
     async listByDescricao(params) {
-        sails.getDatastore().sendNativeQuery("SELECT * FROM produtos WHERE descricao = '$1';", [params.descricao], function (err, rawResult) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-            console.log(rawResult)
-            return rawResult;
-        });
+        return await sails.getDatastore().sendNativeQuery("SELECT * FROM produtos WHERE descricao = '$1';", [params.descricao])
     },
     async listByValor(params) {
-        sails.getDatastore().sendNativeQuery('SELECT * FROM produtos WHERE valor = $1;', [params.valor], function (err, rawResult) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-            console.log(rawResult)
-            return rawResult;
-        });
+        return await sails.getDatastore().sendNativeQuery('SELECT * FROM produtos WHERE valor = $1;', [params.valor])
     },
     async create(params) {
-        sails.getDatastore().sendNativeQuery('SELECT id FROM produtos ORDER BY id DESC LIMIT 1;', [], function (e, firstResult) {
-            let lastId = firstResult.rows.length == 0 ? 0 : firstResult.rows[0].id
-            sails.getDatastore().sendNativeQuery('INSERT INTO produtos(id, descricao, preco, foto, quantidade) VALUES($1, $2, $3, $4, $5);', [lastId + 1, params.descricao, params.preco, params.foto, params.quantidade], function (err, rawResult) {
-                    if (err) {
-                        console.log(err)
-                        return err
-                    }
-                    console.log(rawResult)
-                    return rawResult;
-            });
-        });
+        let result = await sails.getDatastore().sendNativeQuery('SELECT id FROM produtos ORDER BY id DESC LIMIT 1;')
+        let lastId = result.rowCount == 0 ? 0 : result.rows[0].id
+        return await sails.getDatastore().sendNativeQuery('INSERT INTO produtos(id, descricao, preco, foto, quantidade) VALUES($1, $2, $3, $4, $5);', [lastId + 1, params.descricao, params.preco, params.foto, params.quantidade])
     },
     async delete(params) {
-        sails.getDatastore().sendNativeQuery('DELETE FROM produtos WHERE id = $1;', [params.id], function (err) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-        });
+        return await sails.getDatastore().sendNativeQuery('DELETE FROM produtos WHERE id = $1;', [params.id])
     },
     async update(params) {
-        sails.getDatastore().sendNativeQuery('UPDATE produtos SET valor = $2 WHERE id = $1;', [params.id, params.valor], function (err, rawResult) {
-            if (err) {
-                console.log(err)
-                return err
-            }
-        });
+        return await sails.getDatastore().sendNativeQuery('UPDATE produtos SET valor = $2 WHERE id = $1;', [params.id, params.valor])
     },
-
 }
