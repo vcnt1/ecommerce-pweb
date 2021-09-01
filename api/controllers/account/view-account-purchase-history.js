b3b66db23cf79af98aebb1f45ee41eb469c4c954
vicente <vicentedpferreira@outlook.com>
2021-09-01 18:53:28 -0300

    por favor

--- api/controllers/account/view-account-purchase-history.js
+++ api/controllers/account/view-account-purchase-history.js
@@ -1,26 +1,30 @@
 module.exports = {
 
 
-  friendlyName: 'View the purchases history',
+    friendlyName: 'View the purchases history',
 
 
-  description: 'Display "Purchase history" page.',
+    description: 'Display "Purchase history" page.',
 
 
-  exits: {
+    exits: {
 
-    success: {
-      viewTemplatePath: 'pages/account/account-purchase-history'
+        success: {
+            viewTemplatePath: 'pages/account/account-purchase-history'
+        }
+
+    },
+
+
+    fn: async function () {
+        let compras = await Compra.getByClienteId(this.req.session.userId)
+
+        console.log(compras)
+        return {
+            compras: compras,
+        };
+
     }
-
-  },
-
-
-  fn: async function () {
-
-    return {};
-
-  }
 
 
 };
--- api/models/Compra.js
+++ api/models/Compra.js
@@ -11,10 +11,46 @@ module.exports = {
             type: 'number',
             columnName: 'cliente_id'
         },
-         dataHora: {
+        dataHora: {
             type: 'ref',
             //columnType: 'datetime',
             columnName: 'data_hora'
-         },
+        },
+    },
+    async getAll() {
+        let query = await sails.getDatastore().sendNativeQuery('SELECT c.id, data_hora, nome, p.descricao, p.preco, cp.quantidade \n' +
+            'FROM compras c \n' +
+            'INNER JOIN clientes cl ON c.cliente_id = cl.id \n' +
+            'INNER JOIN compras_produtos cp ON cp.compra_id = c.id \n' +
+            'INNER JOIN produtos p ON cp.produto_id = p.id;')
+        return query.rows
+    },
+    async getByClienteId(id) {
+        let query = await sails.getDatastore().sendNativeQuery('SELECT c.id, data_hora, nome \n' +
+            'FROM compras c \n' +
+            'INNER JOIN clientes cl ON c.cliente_id = cl.id \n' +
+            'INNER JOIN compras_produtos cp ON cp.compra_id = c.id \n' +
+            'INNER JOIN produtos p ON cp.produto_id = p.id WHERE cl.id = $1;', [id])
+        let compras = query.rows
+        if (compras.length == 0) {
+            return []
+        }
+
+        let comprasComplete = async (compras) => {
+            let c = []
+            compras.map(async (el) => {
+                let produtos = await sails.getDatastore().sendNativeQuery('SELECT descricao, preco, cp.quantidade FROM compras_produtos cp INNER JOIN produtos p ON cp.produto_id = p.id WHERE cp.compra_id = $1', [el.id])
+                el.produtos = produtos.rows
+                c.push(el)
+            })
+            return c
+        }
+
+        let porFavor = await comprasComplete(compras)
+        return porFavor
+    },
+    async delete(id) {
+        await sails.getDatastore().sendNativeQuery('DELETE FROM categorias_produtos WHERE categoria_id = $1;', [id])
+        await Categoria.destroyOne({id: id})
     },
 };
