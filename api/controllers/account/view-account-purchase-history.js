2f7d72ab074b48be41eda36670f496f99db73090
unknown <gsvieira08@gmail.com>
2021-09-01 18:05:50 -0300

    Acesso da pagina ok

---
+++ api/controllers/account/view-account-purchase-history.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+
+  friendlyName: 'View the purchases history',
+
+
+  description: 'Display "Purchase history" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/account/account-purchase-history'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    return {};
+
+  }
+
+
+};
--- config/routes.js
+++ config/routes.js
@@ -33,6 +33,7 @@ module.exports.routes = {
   'GET /account':            { action: 'account/view-account-overview' },
   'GET /account/password':   { action: 'account/view-edit-password' },
   'GET /account/profile':    { action: 'account/view-edit-profile' },
+  'GET /account/purchasehistory':    { action: 'account/view-account-purchase-history'},
 
   '/logout':                  '/api/v1/account/logout',
   '/exclude':                  '/api/v1/account/exclude',
